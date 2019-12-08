84231678975fe815c2df619af69bbc943330a1cf
jjwarren44 <jjwarren44@gmail.com>
2019-12-08 15:09:11 -0500

    start screen

---
+++ settings.py
@@ -0,0 +1,14 @@
+# Screen settings
+WIDTH, HEIGHT = 448, 596 # Double original pacman display size
+FPS = 60
+
+# Color settings
+BLACK = (0,0,0)
+
+# Font settings
+START_TEXT_SIZE = 28
+START_FONT = 'arial black'
+
+
+# Player settings
+# Mob settings
